b48b78acc6768f2129ad3189d5b8c96a6adf8945
RoyaAmirii <roya.amiri@ada.ac.uk>
2024-10-24 09:25:22 +0000

    added more names

--- oop.js
+++ oop.js
@@ -66,12 +66,21 @@ class AdaStaff extends Person {
 
 const aqil = new Person("Aqil Hussain", "01/01/2000", "Manchester");
 const steve = new AdaStaff("Steve Rich", "01/01/1998", "Lincoln", "Lecturer", "Manchester");
+const roya = new Person("Roya Amiri", "08/07/2005", "Blackburn")
+const zainab = new Person("zainab", "20/03/2006", "London")
+const Aida = new Person("Aida", "13/06/2006", "London")
 
 
 console.log(steve);
 console.log(aqil);
+console.log(roya);
+console.log(zainab);
+console.log(Aida)
 console.log(steve.talk());
 console.log(aqil.talk());
+console.log(roya.talk())
+console.log(zainab.talk())
+console.log(Aida.talk())
 console.log(steve.name);
 steve.name = "Stevano Rich";
 console.log(steve.name);
